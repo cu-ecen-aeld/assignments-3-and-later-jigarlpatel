5310b66677cafb8d2ff40dfb54e3ec7abadabdc3
jigarlpatel <jigarlpatel@gmail.com>
2024-03-22 14:35:18 -0500

    Root causing Assignment 4 failuer

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,23 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 
-# Check if required arguments are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <filesdir> <searchstr>"
-    exit 1
-fi
+filesdir=$1 # path to a directory on the filesystem
+searchstr=$2 # text string which will be searched
 
-filesdir=$1
-searchstr=$2
+# check parameters
+if [ $# -lt 2 ]
+then
+	echo "Fail --> need to put 2 parameters"
+	exit 1
+else
+	if ! [ -d $filesdir ]
+	then
+		echo "Invalid directory path"
+		exit 1
+	else
+		if [ -z $searchstr ]
+		then
+			echo "Inalid search string"
+			exit 1
+		fi
+	fi
+fi 
 
-# Check if filesdir exists and is a directory
-if [ ! -d "$filesdir" ]; then
-    echo "$filesdir is not a directory or does not exist"
-    exit 1
-fi
+# count number of files in director
+number_of_files=$(find $filesdir -type f | wc -l)
 
-# Find matching lines and count them
-num_files=$(find "$filesdir" -type f | wc -l)
-num_matches=$(grep -r "$searchstr" "$filesdir" | wc -l)
+# count matching lines found in respective files
+number_of_matching=$(grep -Rw $filesdir -e $searchstr | wc -l)
 
-echo "The number of files are $num_files and the number of matching lines are $num_matches"
-
+echo "The number of files are $number_of_files and the number of matching lines are $number_of_matching"
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -33,21 +33,18 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
+    git apply ${FINDER_APP_DIR}/dtc-lexer.patch
 
     # TODO: Add your kernel build steps here
-    echo "mrproper"
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
-    echo "defconfig"
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
-    echo "make all"
-    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
-    # make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
-    echo "make dtbs"
-    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
 fi
 
-cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -58,9 +55,8 @@ then
 fi
 
 # TODO: Create necessary base directories
-cd ${OUTDIR}
-mkdir -p rootfs
-cd rootfs
+echo "Create necessary base directories"
+mkdir -p ${OUTDIR}/rootfs && cd ${OUTDIR}/rootfs
 mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
@@ -68,64 +64,68 @@ mkdir -p var/log
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    echo "Configure busybox"
     make distclean
-    make defconfig    
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
-echo "Make and install busybox"
+echo "Make and install busy box"
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a ${OUTDIR}/busybox/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a ${OUTDIR}/busybox/busybox | grep "Shared library"
+cd ${OUTDIR}/rootfs
+${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
 echo "Add library dependencies to rootfs"
-FILE_SRCDIR=$(dirname $(which aarch64-none-linux-gnu-gcc))/..
-cp ${FILE_SRCDIR}/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
-cp ${FILE_SRCDIR}/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
-cp ${FILE_SRCDIR}/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
-cp ${FILE_SRCDIR}/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
+TOOLCHAIN_SYSROOT=$(${CROSS_COMPILE}gcc --print-sysroot)
+cp ${TOOLCHAIN_SYSROOT}/lib/ld-linux-aarch64.so.* ${OUTDIR}/rootfs/lib
+
+cp ${TOOLCHAIN_SYSROOT}/lib64/libm.so.* ${OUTDIR}/rootfs/lib64
+cp ${TOOLCHAIN_SYSROOT}/lib64/libresolv.so.* ${OUTDIR}/rootfs/lib64
+cp ${TOOLCHAIN_SYSROOT}/lib64/libc.so.* ${OUTDIR}/rootfs/lib64
 
 # TODO: Make device nodes
-echo "Make device nodes"
-cd ${OUTDIR}/rootfs/
+echo "Create null device"
 sudo mknod -m 666 dev/null c 1 3
-sudo mknod -m 666 dev/console c 5 1
+
+echo "Create console device"
+sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
-echo "Clean Build Writer utility"
+echo "Clean and build the writer utility"
 cd ${FINDER_APP_DIR}
 make clean
-make CROSS_COMPILE=aarch64-none-linux-gnu-
-cp writer ${OUTDIR}/rootfs/home/
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-echo "Copy Scripts"
-cp finder.sh ${OUTDIR}/rootfs/home/
-cp finder-test.sh ${OUTDIR}/rootfs/home/
-cp autorun-qemu.sh ${OUTDIR}/rootfs/home/
-cd ..
-mkdir -p ${OUTDIR}/rootfs/home/conf
-cp conf/assignment.txt ${OUTDIR}/rootfs/home/conf/
-cp conf/username.txt ${OUTDIR}/rootfs/home/conf/
+echo "Copy scrips and executables to rootfs/home"
+cp finder-test.sh finder.sh writer ${OUTDIR}/rootfs/home
+
+mkdir ${OUTDIR}/rootfs/home/conf
+cp conf/username.txt conf/assignment.txt ${OUTDIR}/rootfs/home/conf
+
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
 
 # TODO: Chown the root directory
 echo "Chown the root directory"
-sudo chown -R root:root ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs/
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
-cd ${OUTDIR}/rootfs
+echo "Create initramfs"
+cd ${OUTDIR}/rootfs/
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+
 cd ${OUTDIR}
-gzip -f initramfs.cpio
-echo "Complete all steps"
+gzip -f initramfs.cpio   
